ced2fca7031ccca729bbc5bcc785b7d78490fbea
Will-I-Ham <arnav@robolabs.org>
2026-01-24 08:45:20 -0800

    jhvjfh

--- src/main.cpp
+++ src/main.cpp
@@ -178,10 +178,10 @@ void autonomous()
   to be consistent
   
   */
-  // LeftSideSuperRush();
+  LeftSideSuperRush();
   // ez::as::auton_selector.selected_auton_call();  // Calls selected auton from autonomous selector
   // RightSideSuperRush();
-   Skills();
+   //Skills()
 }
 
 /**
